94431bd52cab155a47d5606029ba750952654a68
potocki <k255@gmx.com>
2016-08-12 13:11:00 +0200

    Ruby stdin/stdout w. message send/receive

--- src/ruby-cmd-iface/cmd-iface.rb
+++ src/ruby-cmd-iface/cmd-iface.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby -i
 require 'nsq'
 
 NSQ_ADDRESS='127.0.0.1:4150'
@@ -14,10 +15,33 @@ response = Nsq::Consumer.new(
 )
 
 
-request.write('test payload')
-request.terminate()
+counter = 0
+count = 0
+results = Array.new
 
-msg = response.pop
-puts msg.body
-msg.finish
-response.terminate
+ARGF.each_line do |line|
+    if counter == 0
+      puts "Input:"
+      counter = line.to_i
+      count = counter
+    else
+      request.write(line)
+
+      msg = response.pop
+      results.push(msg.body)
+      msg.finish
+
+      counter = counter - 1
+      if counter == 0 && count > 0
+        puts ""
+        puts "Output:"
+        for res in results
+          puts "res: " + res
+        end
+        count = 0
+        break
+      end
+    end
+end
+
+__END__
